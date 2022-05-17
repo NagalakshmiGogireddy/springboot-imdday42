1d5a3c6f5bffe9c9d82ad421a5368c4467663b46
Nagalakshmigogireddy <nagalakshmi.gogireddy@jobiak.com>
2022-05-17 12:23:55 +0530

    completed

---
+++ springboot-imdday42/src/main/java/com/jobiak/imd/Springbootimdday42Application.java
@@ -0,0 +1,13 @@
+package com.jobiak.imd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootimdday42Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootimdday42Application.class, args);
+	}
+
+}
